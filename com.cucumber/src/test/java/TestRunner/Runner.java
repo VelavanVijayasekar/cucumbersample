fd5dbe634f05bfc8d1e92e5ec12062fee9bafaac
Avighna <Avighna@DESKTOP-3NCUCEE>
2021-06-09 22:44:03 +0530

    Feature File Path updated

--- com.cucumber/src/test/java/TestRunner/Runner.java
+++ com.cucumber/src/test/java/TestRunner/Runner.java
@@ -6,7 +6,7 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "C:\\TestLeaf\\eclipse-workspace\\com.cucumber\\src\\test\\resources\\Features\\FeatureFile.feature", glue = {
+@CucumberOptions(features = "./src/test/resources/Features/FeatureFile.feature", glue = {
 		"stepDefenition" }, plugin = { "pretty", "html:target/CucumberReports/report.html",
 				"json:target/CucumberReports/report.json",
 				"junit:target/CucumberReports/report.xml" }, monochrome = true
